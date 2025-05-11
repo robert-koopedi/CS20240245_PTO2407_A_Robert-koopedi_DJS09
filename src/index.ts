37be7745ff66c9add1956c8a51c8e76f564fcc6c
robert koopedi <koopedi05@gmail.com>
2025-05-11 16:23:10 +0200

    added permissions

--- src/index.ts
+++ src/index.ts
@@ -39,19 +39,37 @@ const reviews : {
 showReviewTotal(reviews.length , reviews[0].name, reviews[0].loyaltyUser);
 
 
-const you: {
-  firstName: string;
-    lastName: string;
-    isReturning: boolean;
-    age: number;
-    stayedAt: string []
-} = {
+// const you: {
+//   firstName: string;
+//     lastName: string;
+//     isReturning: boolean;
+//     age: number;
+//     stayedAt: string []
+// } = {
+//   firstName: 'Bobby',
+//   lastName: 'Brown',
+//   isReturning: true,
+//   age: 40,
+//   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+// }
+
+const ADMIN = 'admin'
+const READ_ONLY = 'read-only'
+
+enum Permissions {
+  ADMIN,
+  READ_ONLY
+}
+
+const you = {
   firstName: 'Bobby',
   lastName: 'Brown',
+  permissions: Permissions.ADMIN,
   isReturning: true,
-  age: 40,
+  age: 35,
   stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
+
 
 const properties : {
   image: string;
