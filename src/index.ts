9bac59453e661f8c1b75d1bea27c21870f1eb2e9
robert koopedi <koopedi05@gmail.com>
2025-05-11 13:00:09 +0200

    fixed navbar

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@
 // all TypeScript weakness flags.
 // : number
 const reviewTotalDisplay = document.querySelector("#reviews") 
+const returningUserDisplay = document.querySelector('#returning-user')
+const userNameDisplay = document.querySelector('#user')
 
 const reviews = [
   {
@@ -32,3 +34,20 @@ function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean)
 }
 
 showReviewTotal(reviews.length , reviews[0].name, reviews[0].loyaltyUser);
+
+
+const you = {
+    userName: 'Bobby',
+    isReturning: true,
+}
+
+
+function populateUser(isReturning : boolean, userName : string ) {
+  if (isReturning){
+      returningUserDisplay.innerHTML = 'back'
+  }
+  userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
+
