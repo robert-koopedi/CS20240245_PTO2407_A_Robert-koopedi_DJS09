6ec2b3970327fa0a8265e23fe5aa059db0fecbfe
robert koopedi <koopedi05@gmail.com>
2025-05-11 15:54:04 +0200

    added contact number

--- src/index.ts
+++ src/index.ts
@@ -62,7 +62,7 @@ const properties : {
       code: number;
       country: string;
   };
-  contact: string;
+  contact: [number, string];
   isAvailable: boolean;
 }[] = [
   {
@@ -75,7 +75,7 @@ const properties : {
         code: 45632,
         country: 'Colombia'
   },
-  contact: 'marywinkle@gmail.com',
+  contact:[ +1123495082908, 'marywinkle@gmail.com'],
   isAvailable: true 
 },
 {
@@ -88,7 +88,7 @@ const properties : {
       code: 343903,
       country: 'Poland'
   },
-  contact: 'garydavis@hotmail.com',
+  contact: [ +1123495082908, 'garydavis@hotmail.com'],
   isAvailable: false 
 },
 {
@@ -101,7 +101,7 @@ const properties : {
       code: 35433,
       country: 'United Kingdom',
   },
-  contact: 'andyluger@aol.com',
+  contact: [ +1123495082908, 'andyluger@aol.com'],
   isAvailable: true
 }
 ]
